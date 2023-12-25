dc35b2c2d1fba98082fbbfacbd3ee7b4ddde4369
liuqiao <1459869312@qq.com>
2023-12-26 02:18:48 +0800

    可以运行但是corridor生成有问题

---
+++ build/CMakeFiles/Btraj.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/liuqiao/project_vscode/spectral-main/src/cub_extend_3d.cc" "/home/liuqiao/project_vscode/spectral-main/build/CMakeFiles/Btraj.dir/src/cub_extend_3d.cc.o"
+  "/home/liuqiao/project_vscode/spectral-main/src/cub_wrapper.cpp" "/home/liuqiao/project_vscode/spectral-main/build/CMakeFiles/Btraj.dir/src/cub_wrapper.cpp.o"
+  "/home/liuqiao/project_vscode/spectral-main/src/cuboid_3d.cc" "/home/liuqiao/project_vscode/spectral-main/build/CMakeFiles/Btraj.dir/src/cuboid_3d.cc.o"
+  "/home/liuqiao/project_vscode/spectral-main/src/piecewise_jerk_problem.cc" "/home/liuqiao/project_vscode/spectral-main/build/CMakeFiles/Btraj.dir/src/piecewise_jerk_problem.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Btraj.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Btraj.dir/src/cub_extend_3d.cc.o"
+  "CMakeFiles/Btraj.dir/src/cub_wrapper.cpp.o"
+  "CMakeFiles/Btraj.dir/src/cuboid_3d.cc.o"
+  "CMakeFiles/Btraj.dir/src/piecewise_jerk_problem.cc.o"
+  "bin/Btraj.pdb"
+  "bin/Btraj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Btraj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
